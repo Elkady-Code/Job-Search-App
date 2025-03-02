059496b5cbed454494d1551aec8a0ccaa9190d70
Ibrahim Elkady <97796164+Elkady-Code@users.noreply.github.com>
2025-03-02 13:02:07 +0200

    Deploy
    Deploy on Vercel

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve("config/.env") });
+
 import express from "express";
 import bootstrap from "./src/app.controller.js";
-const app = express();
-const port = process.env.PORT || 3000;
 
+const app = express();
 bootstrap(app, express);
 
-app.listen(port, () => {
-  console.log(`Server is running at Port: ${port}`);
-});
+export default (req, res) => {
+  return app(req, res); 
+};
